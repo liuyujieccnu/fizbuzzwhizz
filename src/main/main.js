4fb4b0f181cb13a46f47fd0e536b61cd050b9bb7
liuyujieccnu <liuyujie@mails.ccnu.edu.cn>
2017-07-31 23:34:59 +0800

    fizbuzzwhizz完成

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,47 @@
 'use strict';
 
-function fizbuzzwhizz(a, b, c){
-  // write your code here...
+function contains(a, i) {
+    return i.toString().includes(a); //if...else...的简写
+}
+
+function threeMultiples(a, b, c, i) {
+    return i % (a * b * c) === 0;
+
+}
+
+function twoMultiples(a, b, i) {
+    return i % (a * b) === 0;
+}
+
+/**
+ * @return {boolean}
+ */
+function Multiples(a, i) {
+    return i % a === 0;
+}
+
+function fizbuzzwhizz(a, b, c) {
+    let result = [];
+    for (let i = 1; i < 101; i++) {
+        if (contains(a, i)) {
+            result.push('Fizz');
+        } else if (threeMultiples(a, b, c, i)) {
+            result.push('FizzBuzzWhizz');
+        } else if (twoMultiples(a, b, i)) {
+            result.push('FizzBuzz');
+        } else if (twoMultiples(a, c, i)) {
+            result.push('FizzWhizz');
+        } else if (twoMultiples(b, c, i)) {
+            result.push('BuzzWhizz');
+        } else if (Multiples(a, i)) {
+            result.push('Fizz');
+        } else if (Multiples(b, i)) {
+            result.push('Buzz');
+        } else if (Multiples(c, i)) {
+            result.push('Whizz');
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
 }
--- src/test/main-test.js
+++ src/test/main-test.js
@@ -2,8 +2,41 @@
 
 describe('fizbuzzwhizz', () => {
 
-  // write your tests here...
-  it('should do something when some condition', () => {
-    expect(false).toEqual(true);
-  });
+    const spNum1 = 3;
+    const spNum2 = 5;
+    const spNum3 = 7;
+    const result = fizbuzzwhizz(spNum1, spNum2, spNum3);
+
+    // write your tests here...
+    it('第3个学生报数结果正确', () => {
+        expect(result[3 - 1]).toEqual('Fizz');
+    });
+
+    it('第5个学生报数结果正确', () => {
+        expect(result[5 - 1]).toEqual('Buzz');
+    });
+
+    it('第7个学生报数结果正确', () => {
+        expect(result[7 - 1]).toEqual('Whizz');
+    });
+
+    it('第9个学生报数结果正确', () => {
+        expect(result[9 - 1]).toEqual('Fizz');
+    });
+
+    it('第15个学生报数结果正确', () => {
+        expect(result[15 - 1]).toEqual('FizzBuzz');
+    });
+
+    it('第21个学生报数结果正确', () => {
+        expect(result[21 - 1]).toEqual('FizzWhizz');
+    });
+
+    it('第35个学生报数结果正确', () => {
+        expect(result[35 - 1]).toEqual('Fizz');
+    });
+
+    it('第63个学生报数结果正确', () => {
+        expect(result[63 - 1]).toEqual('Fizz');
+    });
 });
